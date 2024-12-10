0d29e1be4aef985d0f0a2e83e446984e51ce18fe
Jordan Roberty <jordan.roberty@outlook.fr>
2024-12-10 16:36:56 +0100

    fix direct mode in MixWidget

--- src/Widgets/MixWidget.cpp
+++ src/Widgets/MixWidget.cpp
@@ -206,6 +206,8 @@ void MixWidget::paintGL()
   else
   {
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     m_mainProgram.bind();
     setDepthPeelingUniforms(m_mainProgram, 0);
     renderGLTF(m_mainProgram);
